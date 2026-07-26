58ff7f57fe7df3445e64e2a92fbabfdaa62754d5
Phichayut 'Florentin' Sakwiset <florentin@wemake.cx>
2026-07-26 04:10:58 +0200

    feat: add cloudflare pages deployment adapter
    install @astrojs/cloudflare dependency and configure the adapter in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://florentin.dev/",
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   }
